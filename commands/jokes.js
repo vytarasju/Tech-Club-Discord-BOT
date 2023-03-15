460952d64e95ac8ad1c8eeaa99b2a0a1df7f5ac1
vytarasju <juraskavytaras@gmail.com>
2023-03-15 21:04:09 +0800

    returned deploy-commands

--- commands/jokes.js
+++ commands/jokes.js
@@ -2,8 +2,8 @@ const {SlashCommandBuilder} = require('discord.js');
 const {generateJoke}=require('../joke_generator.js');
 
 module.exports={
-    data:new SlashCommandBuilder().setName('joke').setDescription('Replies with a joke'),
-    async execute(interaction){
-        await interaction.reply(generateJoke());
-    },
+	data:new SlashCommandBuilder().setName('joke').setDescription('Replies with a joke'),
+	async execute(interaction){
+			await interaction.reply(generateJoke());
+	},
 };
